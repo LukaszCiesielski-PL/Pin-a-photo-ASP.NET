f011bf9ba291cb86af7a064bbc31173590f864fc
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2021-11-09 03:54:42 +0100

    Create CRUD without UPDATE
    New functionalities:
    - login form (not working yet)
    - form to create new users (includes validation)
    - list and counter of users
    - possibility to delete users

---
+++ Projekt/Projekt/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Projekt.Controllers
+{
+    public class UserController : Controller
+    {
+        static List<User> Users = new List<User>()
+        {
+            new User()
+            {    
+                //Id = 1,
+                Login = "Clukasz_99",
+                Email = "ciesielski188@gmail.com",
+                Password = "Genesis123!",
+                RepeatPassword = "Genesis123!"
+            },
+            new User()
+            {   
+                //Id = 2,
+                Login = "Clukasz_999",
+                Email = "ciesielski1888@gmail.com",
+                Password = "Genesis1233!",
+                RepeatPassword = "Genesis1233!"
+            }
+
+        };
+        //public static int counter = Users.Count;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            //counter++;
+            return View();
+        }
+
+        public IActionResult AddUser(User user)
+        {
+            if (user.Password == user.RepeatPassword && ModelState.IsValid)
+            {
+                //user.Id = counter;
+                Users.Add(user);
+                return View("UserList", Users); 
+            }            
+            else
+            {
+                return View("Add");
+            }
+
+
+        }
+        
+        public IActionResult Delete(Guid Id)
+        {
+            var RemoveUser = Users.FirstOrDefault(el => Guid.Equals(Id, el.UID));
+            if (RemoveUser != null)
+                Users.Remove(RemoveUser);
+            return View("UserList", Users);
+        }
+
+        public IActionResult Edit(User user)
+        {
+            return View("Add");
+        }
+    }
+}
---
+++ Projekt/Projekt/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Projekt.Models;
+using System.Collections.Generic;
+using System;
+namespace Projekt.Models
+{
+    public class User
+    {
+
+        public readonly Guid UID = Guid.NewGuid();
+
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "Twój login powinien posiadać minimum 5 znaków.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9\s-_]*$", ErrorMessage = "Twój login powinien zaczynać się z WIELKIEJ LITERY. Dostępne znaki specjalne: ' -, _ '")]
+        [Required(ErrorMessage = "Musisz podać login")]
+        public string Login { get; set; }
+
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage = "Twój email jest błędny.")]
+        [Required(ErrorMessage = "Musisz podać email")]
+        public string Email { get; set; }
+
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Twoje hasło powinno posiadać minimum 8 znaków.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Twoje hasło powinno zawirać małe litery, duże litery, cyfry, znaki specjalne")]
+        [Required(ErrorMessage = "Musisz podać hasło")]
+        public string Password { get; set; }
+
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "Twoje hasło powinno posiadać minimum 8 znaków i zgadzać się z pierwotnym hasłem.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Twoje hasło powinno zawirać małe litery, duże litery, cyfry, znaki specjalne i zgadzać się w pierwotnie wprowadzonym hasłem.")]
+        [Required(ErrorMessage = "Musisz podać ponownie hasło")]
+        public string RepeatPassword { get; set; }
+
+       
+    }
+    
+}
